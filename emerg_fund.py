749b8566c3f4e02ff42c6fdc91576180fcd2a8e7
suphawadeeth <bunnymusicc@gmail.com>
2024-02-11 21:08:53 +0100

    add new file: emerg_fund.py

---
+++ emerg_fund.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import datetime as dt 
+
+def emergency_fund_calculator(name):
+    df = pd.read_csv(f'{name}.csv')
+    df = df[df["category"].str.contains("Savings & Investments") == False] # drop the "Savings & Investments" if any
+    df['date'] = pd.to_datetime(df['date']) #change data into the datetime type
+    df['year_month'] = df['date'].dt.to_period('M').astype(str) #extract YYYY-MM 
+    df = df[df['date'] < dt.date.today().strftime("%Y-%m")] #filter all data except current month
+    avg_monthly = df.groupby(['year_month']).sum().mean() #calculate average amount each category
+    
+    fam_status = input("""Enter a number that describes your status: \n
+    '1' single with no dependents & a stable income \n 
+    '2' married & both have a stable income \n 
+    '3' married with a single income/single parent\n 
+    '4' irregular income/your job is seasonal \n 
+    '5' someone in your home is chronically ill \n 
+    '6' you or your partner is self-employed, works on commision, has a irregular income \n 
+    """)
+    
+    if fam_status in ['1','2']:
+        emerg_fund = avg_monthly*3
+    else:
+        emerg_fund = avg_monthly*6
+    
+    print("YOUR EMERGENCY FUND: ", emerg_fund)
+
+    return emerg_fund
+
+
+emergency_fund_calculator(name='test')
